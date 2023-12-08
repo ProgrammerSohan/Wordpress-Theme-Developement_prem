daa9a7521c6fc09c7fc4d3c061aee21be718bc29
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-09 00:22:05 +0600

    creating a gulp task for styles-cot17_C

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ import gulp from 'gulp';
 import yargs from 'yargs';
 //import sass from 'gulp-sass';
 import gulpsass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
 
 const PRODUCTION = yargs.argv.prod;
 var sass = require('gulp-sass')(require('sass'));
@@ -10,6 +11,7 @@ var sass = require('gulp-sass')(require('sass'));
 export const styles = () => {
      return gulp.src('src/assets/scss/bundle.scss')
      .pipe(sass().on('error', sass.logError))
+     .pipe(cleanCSS({compatibility: 'ie8'}))
      .pipe(gulp.dest('dist/asset/css'));
 }
 //export default hello;
