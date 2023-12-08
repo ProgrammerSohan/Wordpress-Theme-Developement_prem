cefd9518dcb3442132a5a77b2318f2ba5dc89139
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-08 23:45:24 +0600

    creating a gulp task for styles-cot17

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,8 +4,7 @@ import yargs from 'yargs';
 
 const PRODUCTION = yargs.argv.prod;
 
-export const hello = (done) => {
-    console.log('PRODUCTION');
-    done();
+export const styles = (done) => {
+       done();
 }
 export default hello;
