adc4fe3215e187f030f79290a77e1b7bd5966866
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-09 00:08:13 +0600

    creating a gulp task for styles-cot17_B

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,15 @@
 
 import gulp from 'gulp';
 import yargs from 'yargs';
+//import sass from 'gulp-sass';
+import gulpsass from 'gulp-sass';
 
 const PRODUCTION = yargs.argv.prod;
+var sass = require('gulp-sass')(require('sass'));
 
-export const styles = (done) => {
-       done();
+export const styles = () => {
+     return gulp.src('src/assets/scss/bundle.scss')
+     .pipe(sass().on('error', sass.logError))
+     .pipe(gulp.dest('dist/asset/css'));
 }
-export default hello;
+//export default hello;
