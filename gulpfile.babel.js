8cbec0c3938e0175a5a8ad75c79e9fac30ccea3e
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-09 23:28:34 +0600

    creating a gulp task for styles cont-cot18_B

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,12 +10,24 @@ import sourcemaps from 'gulp-sourcemaps';
 const PRODUCTION = yargs.argv.prod;
 var sass = require('gulp-sass')(require('sass'));
 
+const paths = {
+     styles: {
+          src:['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+          dest: 'dist/assets/css'
+               
+     }
+}
+
 export const styles = () => {
-     return gulp.src(['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'])
+     return gulp.src(paths.styles.src)
      .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
      .pipe(sass().on('error', sass.logError))
      .pipe(gulpif(PRODUCTION,cleanCSS({compatibility: 'ie8'})))
      .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
-     .pipe(gulp.dest('dist/assets/css'));
+     .pipe(gulp.dest(paths.styles.dest));
+}
+
+export const watch = () => {
+     gulp.watch('src/assets/scss/**/*.scss', styles);
 }
 //export default hello;
