22f6f470144a74570a81ba8b5cb6337ee8c19fd2
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-09 22:17:05 +0600

    creating a gulp task for styles cont-cot18_A

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,14 +5,17 @@ import yargs from 'yargs';
 import gulpsass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
 
 const PRODUCTION = yargs.argv.prod;
 var sass = require('gulp-sass')(require('sass'));
 
 export const styles = () => {
-     return gulp.src('src/assets/scss/bundle.scss')
+     return gulp.src(['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'])
+     .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
      .pipe(sass().on('error', sass.logError))
      .pipe(gulpif(PRODUCTION,cleanCSS({compatibility: 'ie8'})))
+     .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
      .pipe(gulp.dest('dist/assets/css'));
 }
 //export default hello;
